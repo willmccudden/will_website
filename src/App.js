1b4244569c974a202d5f12a6f8d0aaca9744bfb4
William McCudden-Hughes <willmccudden@gmail.com>
2024-02-19 16:06:21 +1100

    routing fix #2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import HomePage from './Components/HomePage/HomePage';
 import ProjectLog from './Components/ProjectLog/ProjectLog';
 import Footer from './Components/HomePage/Footer'
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App (){
   return (
     <div>
     <Routes>
-      <Route path = "/will_website" element = {<HomePage/>}/>
-      <Route path = "/projectlog" element = {<ProjectLog/>}/>
+      <HashRouter path = "/" element = {<HomePage/>}/>
+      <HashRouter path = "/projectlog" element = {<ProjectLog/>}/>
     </Routes>
     <Footer/>
     </div>
