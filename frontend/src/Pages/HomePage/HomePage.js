52303cffd043f3659ba9df8ba7d8cc316a5d76bb
William McCudden-Hughes <willmccudden@gmail.com>
2024-03-12 10:46:53 +1100

    minor chages

--- frontend/src/Pages/HomePage/HomePage.js
+++ frontend/src/Pages/HomePage/HomePage.js
@@ -12,66 +12,51 @@ import { View } from 'react-native-web';
 import UniversityPhoto from '../../Images/UniversityPhoto.jpg'
 import IBMLogo from '../../Images/IBMLogo.png'
 import SurfingPhoto from '../../Images/SurfingPhoto.jpg'
+import HeroPhoto from '../../Images/HeroPhoto.jpg'
+import { useState } from 'react';
 
-
-const WholePage = styled.div`
-    display: flex;
-    // padding: 6em
-    // height: 305px
-    // opacity: ${props => props.hiddenValue}
-`;
-
-const Column = styled.div`
-    flex: ${props => props.weight};
-    // padding: 1em;
-`;
 
 function HomePage() {
 
     return (
         <div>
-            <HeroSection>   
-
-                <NavBar/>
-                <div className="heroInner">
-                    <span>
-                        <h1>Will McCudden</h1>
-                    </span>
+            <div className='container'>
+                <div className='navbar-grid'>
+                    <NavBar />
                 </div>
-            </HeroSection>
-            <WholePage>
-                <Column weight={6}>
+                <div>
+                    <h2>Will McCudden</h2>
+                </div>
 
-                    <h1>---- MY JOURNEY ----</h1>
-                    <Reveal>
-                        <HomePageSlide leftWeight={1} rightWeight={4}>
-                            <img src = {UniversityPhoto} width='100%' alt = 'University Photo'/>
-                            <p className='slide-text'>{HomePageText1}</p>
-                        </HomePageSlide>
-                    </Reveal>
+            </div>
+            <h1>---- MY JOURNEY ----</h1>
+            <Reveal>
+                <HomePageSlide leftWeight={1} rightWeight={4}>
+                    <img src={UniversityPhoto} width='100%' alt='University Photo' />
+                    <p className='slide-text'>{HomePageText1}</p>
+                </HomePageSlide>
+            </Reveal>
 
-                    <Reveal>
-                        <HomePageSlide leftWeight={1} rightWeight={4}>
-                            <img className = 'slide-image' src = {IBMLogo} width='100%' alt = 'IBM Logo'/>
-                            <p className='slide-text'>{HomePageText2}</p>
-                        </HomePageSlide>
-                    </Reveal>
+            <Reveal>
+                <HomePageSlide leftWeight={1} rightWeight={4}>
+                    <img className='slide-image' src={IBMLogo} width='100%' alt='IBM Logo' />
+                    <p className='slide-text'>{HomePageText2}</p>
+                </HomePageSlide>
+            </Reveal>
 
-                    <Reveal>
-                        <HomePageSlide leftWeight={1} rightWeight={4}>
-                            <img className = 'slide-image'src = {SurfingPhoto} width='100%' alt = 'Surfing Photo'/>
-                            <p className='slide-text'>{HomePageText3}</p>
-                        </HomePageSlide>
-                    </Reveal>
+            <Reveal>
+                <HomePageSlide leftWeight={1} rightWeight={4}>
+                    <img className='slide-image' src={SurfingPhoto} width='100%' alt='Surfing Photo' />
+                    <p className='slide-text'>{HomePageText3}</p>
+                </HomePageSlide>
+            </Reveal>
 
-                    <h1>---- MY WEBSITE ----</h1>
+            <h1>---- MY WEBSITE ----</h1>
 
-                    <p>This website is an oppurtunity for me to learn front-end, and in future add back-end functionality, in the hopes of making myself more employable
-                        whilst also keeping myself occupied while searching for work opportunities. I'm keeping track of the website's development <a href='https://willmccudden.atlassian.net/jira/software/projects/WW/boards/2'>here</a> and the whole project is open source in my git repo <a href='https://github.com/willmccudden/will_website'>here</a>
+            <p>This website is an oppurtunity for me to learn front-end, and in future add back-end functionality, in the hopes of making myself more employable
+                whilst also keeping myself occupied while searching for work opportunities. I'm keeping track of the website's development <a href='https://willmccudden.atlassian.net/jira/software/projects/WW/boards/2'>here</a> and the whole project is open source in my git repo <a href='https://github.com/willmccudden/will_website'>here</a>
 
-                    </p>
-                </Column>
-            </WholePage>
+            </p>
         </div>
     )
 }
